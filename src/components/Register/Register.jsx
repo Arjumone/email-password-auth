fac86ebc8825fb9dc12c6e2304b009d15ca53c81
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-05-05 16:43:24 +0600

    added email,password and auth

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile} from 'firebase/auth'
 import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 const auth = getAuth(app)
 
@@ -16,7 +17,8 @@ const Register = () => {
         // 2. collect for data
         const email = event.target.email.value;
         const password = event.target.password.value;
-       console.log(email,password);
+        const name = event.target.name.value;
+       console.log(name,email,password);
     //    validate
        if(!/(?=.*[A-Z])/.test(password)){
         setError('Please add at lrast one uppercase');
@@ -39,6 +41,8 @@ const Register = () => {
         setError('');
         event.target.reset();
         setSuccess('User has been created successfully');
+        sendVerificationEmail(result.user);
+        updateUserData(result.user,name);
         
     })
     .catch(error =>{
@@ -46,6 +50,25 @@ const Register = () => {
         setError(error.message);
       
     })
+    }
+
+    const sendVerificationEmail = (user) =>{
+        sendEmailVerification(user)
+        .then(result =>{
+            console.log(result);
+            alert('Please verify your email address')
+        })
+    }
+    const updateUserData = (name) =>{
+        updateProfile(user,{
+            displayName: name
+        })
+        .then(()=>{
+            console.log('user name updated');
+        })
+        .catch(error =>{
+            setError(error.message);
+        })
     }
 
     const handleEmailChange = (event) =>{
@@ -60,12 +83,14 @@ const Register = () => {
         <div className='w-50 mx-auto'>
             <h4>Please register</h4>
             <form onSubmit={handleSubmit}>
+                <input className='w-50 mb-4 rounded ps-2'  type="text" name='name' id='name' placeholder='Your Name'required/> <br />
                 <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name='email' id='email' placeholder='Your Email'required/> <br />
                 <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name='password' id='password' placeholder='Your Password' required /> 
                 <br />
                 <input className='btn btn-primary' type="submit" value='Register' />
                
             </form>
+            <p><small>Already have an account? Please <Link to='/login'>Login</Link></small></p>
             <p className='text-danger'>{error}</p>
             <p className='text-success'>{success}</p>
         </div>
